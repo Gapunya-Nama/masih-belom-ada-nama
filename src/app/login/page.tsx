9a67ad9aeae5e2883d91823d5a92b6ffb5be1d06
cahyacoding <cahya.bgg@gmail.com>
2024-11-13 11:35:38 +0700

    Menambah karousel di halaman depan dengan shadcn UI

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,62 @@
-import Login from '@/app/components/login';
+"use client";
 
-const Home: React.FC = () => {
+import DiscordCard from '@/app/components/discordCard';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+  CarouselApi
+} from "@/components/ui/carousel";
+import React from 'react';
+
+export function Home() {
+  const [api, setApi] = React.useState<CarouselApi>();
+  const [current, setCurrent] = React.useState(0);
+  const [count, setCount] = React.useState(0);
+
+  React.useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+
   return (
-    <div>
-      <Login />
+    <div className="relative">
+      <Carousel setApi={setApi}>
+        <CarouselContent>
+          <CarouselItem className="flex justify-center items-center">
+            <DiscordCard />
+          </CarouselItem>
+          <CarouselItem className="flex justify-center items-center">
+            <DiscordCard />
+          </CarouselItem>
+          <CarouselItem className="flex justify-center items-center">
+            <DiscordCard />
+          </CarouselItem>
+        </CarouselContent>
+
+        {/* Next and Previous Buttons */}
+        <CarouselPrevious className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-gray-200 p-2 rounded-full hover:bg-gray-300">
+          Prev
+        </CarouselPrevious>
+        <CarouselNext className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-gray-200 p-2 rounded-full hover:bg-gray-300">
+          Next
+        </CarouselNext>
+      </Carousel>
+
+      {/* Pagination Indicator */}
+      <div className="text-center mt-4">
+        {current} / {count}
+      </div>
     </div>
   );
 };
