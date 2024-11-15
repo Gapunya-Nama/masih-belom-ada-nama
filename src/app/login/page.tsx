20913380021e2b08d897afe32dd322806ae0bae5
cahyacoding <cahya.bgg@gmail.com>
2024-11-15 18:06:09 +0700

    merubah discord

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -96,8 +96,22 @@ export function Home() {
           </CarouselItem>
 
 
-          <CarouselItem className="flex justify-center items-center">
-            <DiscordCard />
+          <CarouselItem className="flex flex-col justify-center items-center">
+          <DiscordCard />
+            <div className="space-x-4">
+            <button
+              onClick={handlePrevious}
+              className="bg-gray-200 px-4 py-2 rounded hover:bg-gray-300"
+            >
+              Previous
+            </button>
+            <button
+              onClick={handleNext}
+              className="bg-gray-200 px-4 py-2 rounded hover:bg-gray-300"
+            >
+              Next
+            </button>
+            </div>
           </CarouselItem>
 
         </CarouselContent>
